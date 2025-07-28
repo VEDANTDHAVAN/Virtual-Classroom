357107f4de88aa5995cb5cdc20505abb41185dd6
VEDANTDHAVAN <vedant.231313108@vcet.edu.in>
2025-07-28 20:33:59 +0530

    added logic to fetch and display all Upcoming, previous Meetings.

--- constants/index.ts
+++ constants/index.ts
@@ -45,3 +45,11 @@ export const sidebarLinks = [
     ],
   },
 ]
+
+export const avatarImages = [
+  '/images/avatar-1.jpg',
+  '/images/avatar-2.jpg',
+  '/images/avatar-3.jpg',
+  '/images/avatar-4.jpg',
+  '/images/avatar-5.jpg',
+]
